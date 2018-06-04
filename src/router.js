9418fd27e8dd448eca15c9e7c56d4eeff589f666
Mike <gimp2312@gmail.com>
2018-06-04 04:01:33 +0200

    add Menu component

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from "vue"
 import Router from "vue-router"
 const Home = () => import("@/views/Home")
+const Menu = () => import("@/views/Menu")
 const About = () => import("@/views/About")
 const Gallery = () => import("@/views/Gallery")
 
@@ -12,6 +13,11 @@ export default new Router({
       path: "/",
       name: "Strona Główna",
       component: Home,
+    },
+    {
+      path: "/menu",
+      name: "Menu",
+      component: Menu,
     },
     {
       path: "/galeria",
---
+++ src/views/Menu.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <h1>This is an menu page</h1>
+    <h2>Test2</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur delectus minima mollitia, beatae, officiis eum neque harum in impedit officia odit cupiditate minus maiores ipsam deserunt a eaque, velit dolores?</p>
+    <p>
+      <span>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor facilis tempore corporis soluta animi maxime distinctio dolorem, incidunt quaerat assumenda, suscipit repellat fugiat quam ea quia magnam laudantium officiis ab.</span>
+      <span>Distinctio quo mollitia dignissimos pariatur obcaecati porro voluptates quisquam placeat natus, deserunt, quasi ipsa ea quod? Voluptas aliquam blanditiis asperiores adipisci numquam maiores aut, consequatur, et harum nulla cumque quis?</span>
+      <span>Qui architecto assumenda fuga? Vero voluptatum cumque nostrum quia reiciendis ullam labore enim quas perspiciatis veniam, dolorum hic ea corrupti voluptatibus quasi consequatur! Doloremque veritatis dolor nostrum, fugit illo dicta.</span>
+      <span>Eaque ratione architecto fuga facere esse. Voluptatum distinctio nihil cumque praesentium ea, aliquid perferendis minima quaerat asperiores quasi, consectetur provident autem molestiae? Eos temporibus optio nostrum voluptates quae laboriosam voluptatem.</span>
+      <span>Commodi saepe nisi recusandae natus a voluptatibus quibusdam eos eligendi odit, repellendus ullam laboriosam. Sed labore explicabo ratione excepturi veniam eos sapiente molestiae ipsum accusantium cum nihil qui, totam praesentium?</span>
+      <span>Libero neque ipsum minima qui minus, pariatur obcaecati iure corporis velit at autem optio aliquid impedit esse sunt ullam ratione nesciunt eum laudantium. Debitis ad nobis dolorem iste error quasi.</span>
+      <span>Non sunt, alias voluptatem laudantium voluptatibus quasi quod laborum voluptas velit asperiores excepturi cupiditate earum. Pariatur inventore unde assumenda sunt dolores ipsam qui! Optio, magni? Inventore quibusdam totam unde asperiores?</span>
+      <span>Sunt laboriosam minima eos aut voluptates atque aliquid, temporibus cumque magni nostrum? Doloremque impedit repellendus odit, eius numquam ratione voluptates nam explicabo, ipsa enim placeat beatae delectus quo quibusdam consequatur!</span>
+      <span>Ipsam consequatur, ex iste necessitatibus porro voluptas neque numquam accusantium distinctio veritatis sint tempore esse, dolorem nisi eligendi odit commodi? Porro tenetur ea voluptatibus alias illum, at enim ipsum esse.</span>
+      <span>Consectetur labore minima nam beatae nesciunt architecto, ea doloremque voluptatibus nihil exercitationem commodi assumenda veniam fuga repudiandae, delectus esse quam officia. Quaerat est consequuntur inventore? Cumque provident explicabo quis rem?</span>
+      <span>Architecto eligendi fugiat odio molestias numquam placeat repudiandae repellendus quia hic deserunt distinctio fuga labore ex laboriosam ad consequuntur nam veniam asperiores enim facilis saepe cupiditate repellat, voluptatem magnam. Temporibus.</span>
+      <span>Officiis, nihil? Deserunt soluta dicta necessitatibus sit nam iste, enim nulla architecto ut voluptatibus minima maxime? Illum fuga facilis suscipit totam maxime a dolorem assumenda asperiores odio? Qui, ab porro.</span>
+      <span>Consequuntur quidem vel eius a molestias nobis enim libero suscipit fugit tempore magnam sint commodi at, quae, error est earum, quo qui ullam atque ad! Ea id at suscipit vitae?</span>
+      <span>Deserunt, hic vel. Voluptatem, commodi perferendis molestias suscipit libero beatae aut modi consequuntur officia, error et eaque ad ipsa voluptatum eos minima corrupti tempora? Velit porro quibusdam est hic amet.</span>
+      <span>Vero pariatur temporibus nisi rem error beatae perferendis quos autem harum, ipsam atque dolor, eaque quod, provident eum tempora vel consequatur ipsum iure minima debitis nam! Non eos esse adipisci.</span>
+      <span>Distinctio veniam laboriosam consequatur exercitationem corporis ipsa qui doloremque rerum quasi. Error, placeat. Temporibus, veniam dolores neque numquam aliquid odit ducimus similique ad nihil error perferendis quas totam magni nesciunt.</span>
+      <span>Magni cumque assumenda eos aliquid similique pariatur, ullam sed, deleniti quo, velit mollitia ratione architecto? Itaque doloribus explicabo dolores incidunt animi possimus? Dolore, vero. Molestias magnam velit consequuntur sequi similique!</span>
+      <span>Nesciunt, incidunt corrupti nisi deleniti, totam doloribus quos voluptatibus molestiae reprehenderit iste dolores, accusantium ipsam exercitationem quod! Eveniet sunt, iusto tempora doloribus magni in aliquid, adipisci, earum optio qui ab.</span>
+      <span>Reprehenderit sunt assumenda quidem non eligendi obcaecati similique vitae explicabo odit dignissimos pariatur iure eaque sapiente delectus optio exercitationem perspiciatis nihil, eveniet quasi recusandae deserunt, harum nisi sequi quam? Consequuntur.</span>
+      <span>Voluptas perferendis ea deleniti, itaque cumque eos doloribus totam animi maiores praesentium officia consectetur ad dolore voluptatem aperiam eveniet architecto? Iste accusamus possimus maxime alias sequi libero excepturi omnis mollitia.</span>
+    </p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.menu {
+  padding: 50px 10px;
+}
+</style>
